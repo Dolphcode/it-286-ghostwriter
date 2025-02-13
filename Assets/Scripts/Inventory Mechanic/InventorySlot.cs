79426a8644ca0f438a48bdc55b47011b2b38d786
aw399 <aw399@njit.edu>
2025-02-13 09:37:25 -0500

    Added Inventory Slot
    INVENTORY SLOT MY BELOVED

---
+++ Assets/Scripts/Inventory Mechanic/InventorySlot.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InventorySlot
+{
+    [SerializeField] private ItemData itemData;
+
+    public ItemData ItemData => itemData;
+    public bool active = false;
+
+    /*
+     * @brief create inventory slot with an item in it
+     */
+    public InventorySlot(ItemData source)
+    {
+        itemData = source;
+        active = true;
+    }
+
+    /*
+     * @brief create an empty inventory slot
+     */
+    public InventorySlot()
+    {
+        itemData = null;
+        active = false;
+    }
+
+    /*
+    * @brief clear the slot -> Used when the item is being moved, removed or used.
+    */
+    public void ClearSlot()
+    {
+        itemData = null;
+        active = false;
+    }
+}
