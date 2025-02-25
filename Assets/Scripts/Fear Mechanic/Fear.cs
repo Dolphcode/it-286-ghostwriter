6b7bc2cfd9f7bd3b28490da9e803fe6eaa1ecf1c
ImGettingInted <rbg22@njit.edu>
2025-02-24 20:03:31 -0500

    bippity boopity

--- Assets/Scripts/Fear Mechanic/Fear.cs
+++ Assets/Scripts/Fear Mechanic/Fear.cs
@@ -8,15 +8,18 @@ public class Fear : MonoBehaviour
     public TMP_Text text;
     public float fearRate;
     [HideInInspector]
-    public bool isSanity0;
-
+    public bool isSanity100;
+    [SerializeField]
+    int minFear;
     void Start()
     {
+
         ///<summary>
-        /// Starts fear at 100%
+        /// Starts fear at 0%
         /// </summary>
-        fearMeter = 100;
-        isSanity0 = false;
+        fearMeter = 0;
+        minFear = 1;
+        isSanity100 = false;
     }
 
     void Update()
@@ -25,10 +28,10 @@ public class Fear : MonoBehaviour
         /// Makes the fear tick down at a constant rate
         /// Modifiable by changing <param> fearRate </param> 
         /// </summary>
-        
-        if (fearMeter > 0)
+        fearMeter = Mathf.Clamp(fearMeter, minFear, 100);
+        if (fearMeter < 100)
         {
-            fearMeter -= Time.deltaTime * fearRate;
+            fearMeter += Time.deltaTime * fearRate;
             fearMeter.ToString();
             text.text = "Fear: " + (int)fearMeter + "%";
         }
@@ -37,9 +40,9 @@ public class Fear : MonoBehaviour
         ///detects if the players sanity drops to 0
         /// </summary>
 
-        if (fearMeter <= 0)
+        if (fearMeter >= 100)
         {
-            isSanity0 = true;
+            isSanity100 = true;
             text.text = "You Blacked Out";
 
 
@@ -48,17 +51,19 @@ public class Fear : MonoBehaviour
         
     }
     /// <summary>
-    /// Changes the rate at which fear depletes. Default is around fearRate = .25 (for now)...
+    /// Changes the rate at which fear depletes.
     /// </summary>
+    /// <param name="newRate"> Default is .25 </param>
     public void ChangeFearRate(float newRate)
         {
-        fearRate = newRate;
+            fearRate = newRate;
+            
         }
 
     /// <summary>
-    /// Changes the fear by the amount that is in the parameter
-    /// (If you want to take away fear, put a negative number in the parameter)
+    /// Changes the fear by the amount that is in the parameter (If you want to take away fear, put a negative number in the parameter)
     /// </summary>
+    /// <param name="fearChange">the amount of fear being changed (fear goes from 0-100) </param>
     public void ChangeFearMeter(float fearChange)
     {
         fearMeter += fearChange;
