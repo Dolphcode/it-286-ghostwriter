f424a015dd922da7cefb875a5262f5046946c392
Dolphcode <bastikulit@gmail.com>
2025-04-04 19:37:26 -0400

    Removed previous action button implementation for simplicity

--- Assets/Scripts/Inventory Mechanic/ButtonAction.cs
+++ Assets/Scripts/Inventory Mechanic/ButtonAction.cs
@@ -3,21 +3,6 @@ using UnityEngine.Events;
 
 public class ButtonAction : PlayerInteractable
 {
-    public enum ButtonType { ComputerButton, ShopItem, WorldButton }
-    
-    public ButtonType buttonType;
-
-    // The UI that will be displayed
-    public GameObject targetUI;
-
-    // All UI objects that will be turned off
-    public GameObject uiOff1;
-    public GameObject uiOff2;
-    public GameObject uiOff3;
-
-    // For shop UI
-    public ItemData itemShop;
-
     public UnityEvent _OnClick;
 
     public override void interact()
@@ -26,22 +11,5 @@ public class ButtonAction : PlayerInteractable
         _OnClick.Invoke();
     }
 
-    // When the button is interacted with, turn on the targeted ui and turn off all other ui screens
-    public void Button_clicked()
-    {
-        // Computer Button
-        if (buttonType == ButtonType.ComputerButton)
-        {
-            // Show the UI of the button
-            targetUI.SetActive(true);
 
-            // Hide all other UI screens
-            uiOff1.SetActive(false); uiOff2.SetActive(false); uiOff3.SetActive(false);
-        }
-        // Shop Item Button
-        else if (buttonType == ButtonType.ShopItem)
-        {
-
-        }
-    }
 }
