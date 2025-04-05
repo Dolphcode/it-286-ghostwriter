b519fb1476577df859221f3b1d34e350aaeee6ee
Dolphcode <bastikulit@gmail.com>
2025-04-04 20:01:41 -0400

    Implement shop in new lobby

--- Assets/Scripts/Core/LevelDataManager.cs
+++ Assets/Scripts/Core/LevelDataManager.cs
@@ -148,6 +148,27 @@ public class LevelDataManager : MonoBehaviour
         return selectedItemCounts[index];
     }
 
+    /// <summary>
+    /// Get the number of a specific type of item owned by the player
+    /// </summary>
+    /// <param name="index">Which item type to check</param>
+    /// <returns>The number of that item that is owned by the player</returns>
+    public int GetOwnedCount(int index)
+    {
+        return itemCounts[index];
+    }
+
+    /// <summary>
+    /// Returns a reference to the ItemData object representing an item of a particular
+    /// index.
+    /// </summary>
+    /// <param name="index">What index we should be retrieving data for</param>
+    /// <returns>The ItemData template</returns>
+    public ItemData GetItemInfo(int index)
+    {
+        return itemTemplates[index];
+    }
+
 
     // Level Loading Functions
     public AsyncInstantiateOperation InstantiateObjects()
