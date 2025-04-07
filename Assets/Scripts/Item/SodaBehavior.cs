eac35c14f2c99101f37359c269c7231d525e3d67
ImGettingInted <rbg22@njit.edu>
2025-04-07 18:42:53 -0400

    lil changes on 4/7/25
    Simple Durability on soda and crucifix
    Fixed sliding on player movement
    Adding infinite Sprinting Mechanic

--- Assets/Scripts/Item/SodaBehavior.cs
+++ Assets/Scripts/Item/SodaBehavior.cs
@@ -5,9 +5,21 @@ public class SodaBehavior : ItemBehavior
 
     public override void Interact()
     {
-        GameObject.Find("Fear").GetComponent<Fear>().ChangeFearMeter(-20);
-
+        
         Debug.Log("Interacting with Soda");
+        
+        if (data.durability > 0)
+        {
+            data.durability -= 1;
+            //GameObject.Find("Fear").GetComponent<Fear>().ChangeFearMeter(-20);
+        }
+        else
+        {
+            
+            Debug.Log("No More Durability");
+        }
+
+        
     }
 
     public override void Load(ItemData itemData)
